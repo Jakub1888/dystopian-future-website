5ffeff445fa5ccc084e46edc171b04eb0631981f
Jakub1888 <krasnan.jk@gmail.com>
2021-01-17 09:36:05 +0100

    Code formatting with extension Prettier

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,90 +1,76 @@
-;(function($, window, document, undefined) {
-	
-	var Lightbox = {
+(function ($, window, document, undefined) {
+  var Lightbox = {
+    image: null,
+    overlay: null,
+    element: null,
+    settings: null,
 
-		image: null,
-		overlay: null,
-		element: null,
-		settings: null,
+    init: function (element, settings) {
+      this.element = element;
+      this.settings = settings;
+      this.overlay = $("#overlay");
 
-		init: function( element, settings ) {
-			this.element  = element;
-			this.settings = settings;
-			this.overlay  = $('#overlay');
+      if (this.overlay.length) this.image = this.overlay.find("img");
+      else this.createOverlay();
 
-			if( this.overlay.length )
-				this.image = this.overlay.find('img');
-			else
-				this.createOverlay();
+      this.attachHandlers();
+    },
 
-			this.attachHandlers();
-		},
+    createOverlay: function () {
+      this.overlay = $("<div/>", { id: "overlay" }).hide();
+      this.image = $("<img/>", { src: "", alt: "" }).appendTo(this.overlay);
 
-		
+      this.overlay.appendTo("body");
+    },
 
-		createOverlay: function() {
+    attachHandlers: function () {
+      this.overlay.on("click", function () {
+        Lightbox.hide();
+      });
 
-			this.overlay = $('<div/>', { id: 'overlay' }).hide();
-			this.image = $('<img/>', { src: '', alt: ''}).appendTo( this.overlay );
+      $(document).on("keyup", function (event) {
+        if (event.which === 27) Lightbox.hide();
+      });
 
-			this.overlay.appendTo('body');
-			
-		},
+      this.element.on("click", function (event) {
+        event.preventDefault();
+        Lightbox.setImage($(this).attr("href"));
+        Lightbox.show();
+      });
+    },
 
-		attachHandlers: function() {
-			
-			this.overlay.on('click', function() {
-				Lightbox.hide();
-			});
+    setImage: function (url) {
+      this.image.attr("src", url);
+    },
 
-			$(document).on('keyup', function(event) {
-				if ( event.which === 27 ) Lightbox.hide();
-			});
+    show: function () {
+      this.overlay[this.settings.showEffect](this.settings.speed);
+    },
 
-			this.element.on('click', function(event) {
-				event.preventDefault();
-				Lightbox.setImage( $(this).attr('href') );
-				Lightbox.show();
-			});
-			
-		},
+    hide: function () {
+      var config = this.settings;
 
-		setImage: function( url ) {
-			this.image.attr('src', url);
-		},
+      this.overlay[config.hideEffect](config.speed, function () {
+        if ($.isFunction(config.complete)) {
+          config.complete.call(this);
+        }
+      });
+    },
+  };
 
-		show: function(){
-			this.overlay[ this.settings.showEffect ]( this.settings.speed );
-		},
+  $.fn.lightbox = function (options) {
+    var settings = $.extend(
+      {
+        speed: 250,
+        showEffect: "fadeIn",
+        hideEffect: "fadeOut",
+        complete: null,
+      },
+      options
+    );
 
-		hide: function(){
-			var config = this.settings;
-
-			this.overlay[ config.hideEffect ]( config.speed, function(){
-				if ( $.isFunction( config.complete ) ) {
-					config.complete.call( this );
-				}
-			});
-		},
-
-	}
-
-	$.fn.lightbox = function(options) {
-
-		var settings = $.extend({
-			speed: 250,
-			showEffect: 'fadeIn',
-			hideEffect: 'fadeOut',
-			complete: null
-		}, options);
-		
-		return this.each(function(){
-			Lightbox.init( $(this), settings );
-		});
-
-	}
-	
-	
-	
+    return this.each(function () {
+      Lightbox.init($(this), settings);
+    });
+  };
 })(jQuery, window, document);
-
--- js/script.js
+++ js/script.js
@@ -1,86 +1,76 @@
+(function ($) {
+  // FADE IN ON SCROLL
+  //-------------------------------------
 
-(function($) {
- 
+  $(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height() - 20;
+    var tags = $(".media");
 
-// FADE IN ON SCROLL
-//-------------------------------------
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
 
-  $(document).on('scroll', function () {
-      var pageTop = $(document).scrollTop()
-      var pageBottom = pageTop + $(window).height() - 20
-      var tags = $('.media')
-    
-      for (var i = 0; i < tags.length; i++) {
-        var tag = tags[i]
-    
-        if ($(tag).position().top < pageBottom) {
-          $(tag).addClass("visible")
-        } else {
-          $(tag).removeClass("visible")
-        }
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
       }
-    });
-
-
-//SELECTED
-//------------------------------------
-
-  var list = $('.controls li')
-
-  list.on('click', function() {
-  
-      $(this).toggleClass('selected').siblings().removeClass('selected');
+    }
   });
 
-//--------------------------------------
-//CONTROLS
-  
-  $('#header-2 ul a').on('click', function(event){
-      event.preventDefault();
+  //SELECTED
+  //------------------------------------
 
-      var a = $(this),
-          href = a.attr('href'),
-          li = a.parent();
+  var list = $(".controls li");
 
-          if ( $('#header-2 ul').find('.selected').is( li ) ) return;
-
-      $('#header-2 ul').find('.selected')
-      li.addClass('.selected')
-      .siblings().removeClass('.selected');
-
-      $('section').removeClass('active');
-
-  
-      $(href).addClass('active').hide().fadeIn(400);
-      
+  list.on("click", function () {
+    $(this).toggleClass("selected").siblings().removeClass("selected");
   });
 
+  //--------------------------------------
+  //CONTROLS
+
+  $("#header-2 ul a").on("click", function (event) {
+    event.preventDefault();
+
+    var a = $(this),
+      href = a.attr("href"),
+      li = a.parent();
+
+    if ($("#header-2 ul").find(".selected").is(li)) return;
+
+    $("#header-2 ul").find(".selected");
+    li.addClass(".selected").siblings().removeClass(".selected");
+
+    $("section").removeClass("active");
+
+    $(href).addClass("active").hide().fadeIn(400);
+  });
 
   //--------------------------------------
   //LIGHTBOX
-    
-  $('section a').lightbox();
-    
-	//--------------------------------------------------
-	// RETURN TO THE TOP
 
-	var returnTop = $('<a>', {
-		href: '#home',
-		class: 'return-top',
-		html: '<i class="fa fa-caret-up fa-5x"></i>'
-	});
+  $("section a").lightbox();
 
-	returnTop
-		.hide()
-		.appendTo('body')
-		.on('click', function () {
-			$('body, html').animate({ scrollTop: 0 }, 500);
-		});
+  //--------------------------------------------------
+  // RETURN TO THE TOP
 
-	var win = $(window);
-	win.on('scroll', function() {
-		if ( win.scrollTop() >=1200 ) returnTop.fadeIn();
-		else returnTop.hide();
+  var returnTop = $("<a>", {
+    href: "#home",
+    class: "return-top",
+    html: '<i class="fa fa-caret-up fa-5x"></i>',
   });
 
+  returnTop
+    .hide()
+    .appendTo("body")
+    .on("click", function () {
+      $("body, html").animate({ scrollTop: 0 }, 500);
+    });
+
+  var win = $(window);
+  win.on("scroll", function () {
+    if (win.scrollTop() >= 1200) returnTop.fadeIn();
+    else returnTop.hide();
+  });
 })(jQuery);
